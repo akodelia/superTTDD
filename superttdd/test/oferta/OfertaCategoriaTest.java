66a4e74364ac23925a9c1a64bdd155bd023a7252
akodelia <ankodelia@gmail.com>
2013-05-26 19:41:29 -0300

    Refactor: OfertaCAtegoriaTest

--- superttdd/test/oferta/OfertaCategoriaTest.java
+++ superttdd/test/oferta/OfertaCategoriaTest.java
@@ -1,6 +1,8 @@
 package superttdd.test.oferta;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Matchers.anyDouble;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -27,20 +29,20 @@ public class OfertaCategoriaTest {
 	@Before
 	public void setUp() {
 		categoria = new CategoriaProducto("Farmacia");
-		otraCategoria = new CategoriaProducto("Almacén");
-		marca = new MarcaProducto("Marca");
+		otraCategoria = new CategoriaProducto("Verdulería");
+		marca = mock(MarcaProducto.class);
 		oferta = new OfertaCategoria(categoria, 15.0);
 		productos = new ArrayList<Producto>();
 	}
 
 	@Test
 	public void  aplicarDescuentoAProducto() {
-		Producto mockProd = spy(new Producto("Producto", marca, categoria, 150.0));
-		productos.add(mockProd);
+		Producto producto = new Producto("Producto", marca, categoria, 150.0);
+		productos.add(producto);
 
 		oferta.aplicarOferta(productos);
 		
-		verify(mockProd, times(1)).setPorcentajeDescuento(anyDouble());
+		assertEquals(oferta.getPorcentajeDescuento(), producto.getPorcentajeDescuento());
 	}
 
 	@Test
@@ -55,4 +57,5 @@ public class OfertaCategoriaTest {
 
 
 }
+
 
