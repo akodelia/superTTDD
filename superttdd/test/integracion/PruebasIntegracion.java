a6c89cdda7c981de998f6f33b49a5f7ddd78784f
Florencia Tristant <flotristant@gmail.com>
2013-05-30 00:23:14 -0300

    agrego paquete y clase de tests de integracion vacia

---
+++ superttdd/test/integracion/PruebasIntegracion.java
@@ -0,0 +1,19 @@
+package superttdd.test.integracion;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PruebasIntegracion {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
