fbdb441d080ee40e1f3c0a35908eafb55e7d4000
Florencia Tristant <flotristant@gmail.com>
2013-05-31 01:31:24 -0300

    correccion aplicacion descuento en producto - agrego validacion

--- superttdd/producto/Producto.java
+++ superttdd/producto/Producto.java
@@ -1,81 +1,86 @@
 package superttdd.producto;
 
 public class Producto implements IProducto {
-	
+
 	private RegistroProducto registroProducto;
 
 	private Double porcentajeDescuento;
-	
+
 	private Double precioActual;
 
 	private Producto(Producto unProducto) {
 		this.registroProducto = unProducto.registroProducto;
 		this.porcentajeDescuento = unProducto.porcentajeDescuento;
-		this.precioActual=unProducto.precioActual;
+		this.precioActual = unProducto.precioActual;
 	}
-	
+
 	public Producto(RegistroProducto registroProducto) {
 		this.registroProducto = registroProducto;
 		this.porcentajeDescuento = 0.0;
-		this.precioActual=registroProducto.getPrecio();
+		this.precioActual = registroProducto.getPrecio();
 	}
-	
+
 	public IProducto clonar() {
 		return new Producto(this);
 	}
-	
+
 	public CategoriaProducto getCategoriaProducto() {
 		return registroProducto.getCategoria();
 	}
-	
+
 	public Double getPorcentajeDescuento() {
 		return porcentajeDescuento;
 	}
-	
+
 	public MarcaProducto getMarca() {
 		return registroProducto.getMarca();
 	}
+
 	@Override
 	public void addPorcentajeDescuento(Double porcentajeDescuento) {
-		Double descuento = this.precioActual*(porcentajeDescuento)/100.0;
-		this.precioActual-=descuento;
-//		this.porcentajeDescuento += porcentajeDescuento ;
-//		if(porcentajeMaximoSuperado()) {
-//			this.porcentajeDescuento = PORCENTAJE_MAX;
-//		}
+		Double descuento = this.precioActual * (porcentajeDescuento) / 100.0;
+		if (descuento < precioActual) {
+			this.precioActual -= descuento;
+		}
+		// this.porcentajeDescuento += porcentajeDescuento ;
+		// if(porcentajeMaximoSuperado()) {
+		// this.porcentajeDescuento = PORCENTAJE_MAX;
+		// }
 	}
 
 	private Boolean porcentajeMaximoSuperado() {
-		if(this.porcentajeDescuento >= PORCENTAJE_MAX) {
+		if (this.porcentajeDescuento >= PORCENTAJE_MAX) {
 			return true;
 		}
 		return false;
 	}
+
 	@Override
 	public String getNombre() {
 		return registroProducto.getNombre();
 	}
+
 	@Override
 	public Double getPrecioBase() {
-		return registroProducto.getPrecio(); 
+		return registroProducto.getPrecio();
 	}
-
 
 	@Override
 	public Double getPrecioFinal() {
-//		Double descuento = registroProducto.getPrecio() * porcentajeDescuento / 100;
-//		
-//		return (registroProducto.getPrecio() - descuento);
+		// Double descuento = registroProducto.getPrecio() * porcentajeDescuento
+		// / 100;
+		//
+		// return (registroProducto.getPrecio() - descuento);
 		return precioActual;
 	}
-	
+
 	public RegistroProducto getRegistroProducto() {
 		return registroProducto;
 	}
 
 	@Override
 	public boolean validarCategoria(CategoriaProducto categoria) {
-		if(this.registroProducto.getCategoria() != null) {
+		if (this.registroProducto.getCategoria() != null) {
 			return this.registroProducto.getCategoria().sonIguales(categoria);
 		}
 		return false;
@@ -83,7 +88,7 @@ public class Producto implements IProducto {
 
 	@Override
 	public boolean validarMarca(MarcaProducto marca) {
-		if(this.registroProducto.getMarca() != null) {
+		if (this.registroProducto.getMarca() != null) {
 			return this.registroProducto.getMarca().sonIguales(marca);
 		}
 		return false;
