6ef640a04375e6e18e6063d38126a7181783f675
Florencia Tristant <flotristant@gmail.com>
2013-06-05 16:28:31 -0300

    agrego ofertas dia para filtrar en DescuentoJubilado

--- superttdd/promociones/DescuentoJubilado.java
+++ superttdd/promociones/DescuentoJubilado.java
@@ -1,19 +1,42 @@
 package superttdd.promociones;
 
-import superttdd.comprobante.Factura;
+import java.util.ArrayList;
+import java.util.List;
 
-public class DescuentoJubilado implements DescuentoFactura{
+import superttdd.comprobante.Factura;
+import superttdd.ofertas.Oferta;
+import superttdd.ofertas.OfertaDia;
+
+public class DescuentoJubilado implements DescuentoFactura {
 
 	private Double porcentaje_descuento;
-	
-	public DescuentoJubilado(Double porcentaje_descuento) {
-		this.porcentaje_descuento=porcentaje_descuento;
+	private List<OfertaDia> ofertas_dia_filtro;
+
+	public DescuentoJubilado(Double porcentaje_descuento,
+			List<OfertaDia> ofertas_dias_filtro) {
+		this.porcentaje_descuento = porcentaje_descuento;
+		if (ofertas_dias_filtro != null) {
+			this.ofertas_dia_filtro = ofertas_dias_filtro;
+		} else {
+			this.ofertas_dia_filtro = new ArrayList<OfertaDia>();
+		}
 	}
-	
+
 	@Override
 	public void aplicarDescuento(Factura factura) {
-		Double monto_descuento=factura.getMontoTotalConDescuentos()*((100-porcentaje_descuento)/100);
-		factura.descontarMonto(monto_descuento);	
+		if (esValidoDescuento()) {
+			Double monto_descuento = factura.getMontoTotalConDescuentos()
+					* ((100 - porcentaje_descuento) / 100);
+			factura.descontarMonto(monto_descuento);
+		}
 	}
-	
+
+	private boolean esValidoDescuento() {
+		for (OfertaDia oferta : ofertas_dia_filtro) {
+			if (oferta.hoyEsDiaDePromo()) {
+				return true;
+			}
+		}
+		return false;
+	}
 }
